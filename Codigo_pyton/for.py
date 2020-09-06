1bea0dddb4a7f708662678a966c80948f325eb5f
Crashint <crashint@hotmail.com>
2020-09-06 15:10:58 -0500

    Ciclo For

---
+++ Codigo_pyton/for.py
@@ -0,0 +1,14 @@
+#print (1)
+#print (2)
+#print (3)
+#print (4)
+#print (5)
+
+#contador = 1
+#print (contador)
+#while contador < 1000 :
+#    contador =+ 1 #Contador =+ 1 es igual a contador = contador + 1 
+#    print(contador)
+    
+for contador in range (1001): #rango toma de 1 hasta el numero antes del final.
+    print (contador)
